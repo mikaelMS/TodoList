72de56174c5856a960571134f190f29aa0411d6f
mikaelMS <schmidt.m.work@googlemail.com>
2019-04-19 18:41:04 +0200

    Added complete state and styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import Proptypes from 'prop-types';
 
 export class Item extends Component {
+  // getStyleOld = () => {
+  //   if (this.props.todo.completed) {
+  //     return {
+  //       textDecoration: 'line-through'
+  //     };
+  //   } else {
+  //     return {
+  //       textDecoration: 'none'
+  //     };
+  //   }
+  // };
+
+  getStyle = () => {
+    return {
+      color: '#fff',
+      backgroundColor: '#333',
+      borderBottom: '1px solid #ccc',
+      padding: '10px',
+      // HOLY SHIT thats smart
+      textDecoration:
+        this.props.todo.completed === true ? 'line-through' : 'none'
+    };
+  };
+
+  markComplete = () => {};
+
   render() {
     return (
-      <div>
-        <p>{this.props.todo.title}</p>
+      <div style={this.getStyle()}>
+        <p>
+          <input type='checkbox' onChange={this.markComplete} />
+          {this.props.todo.title}
+        </p>
       </div>
     );
   }
 }
 
+// PropTypes
+Item.propTypes = {
+  todo: Proptypes.object.isRequired
+};
+
 export default Item;
+
+// Important, because style is in an object
+const itemStyle = {
+  color: '#fff',
+  backgroundColor: '#333'
+};
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Item from './Item';
+import Proptypes from 'prop-types';
 
 class Todos extends Component {
   render() {
     return this.props.todos.map(todo => <Item key={todo.id} todo={todo} />);
   }
 }
+
+// PropTypes
+Todos.propTypes = {
+  todos: Proptypes.array.isRequired
+};
 
 export default Todos;
