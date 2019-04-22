26fd5fcacc79377464e1c128a51958704b61e156
mikaelMS <schmidt.m.work@googlemail.com>
2019-04-22 17:21:43 +0200

    Added About page and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import './App.css';
 import uuid from 'uuid';
 
@@ -64,17 +66,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <div className='container'>
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}
-          />
+      <Router>
+        <div className='App'>
+          <div className='container'>
+            <Header />
+            <Route
+              exact //Andernfalls is second Route with this loading
+              path='/'
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path='/about' component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,19 @@
 // rcf tab possible
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1>My Todo List</h1>
+      <Link to='/' style={linkStyle}>
+        Home
+      </Link>{' '}
+      |
+      <Link to='/about' style={linkStyle}>
+        About
+      </Link>
     </header>
   );
 }
@@ -13,6 +21,11 @@ function Header() {
 const headerStyle = {
   padding: '10px',
   textAlign: 'center'
+};
+
+const linkStyle = {
+  color: '#508EF5',
+  textDecoration: 'none'
 };
 
 export default Header;
